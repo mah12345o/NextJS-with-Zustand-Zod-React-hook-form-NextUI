30c99b22fc2f7f4445c434ea27f54f7612d4da63
Mahesh kumar <107390064+mah12345o@users.noreply.github.com>
2024-06-10 18:28:41 +0530

    -minor changes.

--- src/app/component/Navigation/page.tsx
+++ src/app/component/Navigation/page.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
-
-const Navigation = ({ NavLinks}) => {
+interface NavigationProps {
+  NavLinks: string[];
+}
+const Navigation: React.FC<NavigationProps> = ({ NavLinks }) => {
   return (
     <div className="p-3 bg-orange-300 flex justify-end">
-      {NavLinks.map((el) => (
+      {NavLinks.map((el: string) => (
         <Link key={el} className="mx-2" href={el}>
-          {el==="/Ecommerce/Cart"?"Cart":"/Ecommerce/Products"}
+          {el === "/Ecommerce/Cart" ? "Cart" : "Products"}
         </Link>
       ))}
     </div>
